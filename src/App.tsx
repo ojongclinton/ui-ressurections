56d4f5614eb24f80c3c4ad269181c820686643a0
ojong <82442816+ojongclinton@users.noreply.github.com>
2025-10-22 10:37:48 +0100

    installed packages and initialized quantum-supremacy

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import Router from "./Router"
+
 function App() {
 
   return (
-    <div className='bg-red-600 p-5'>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum, accusantium. Illum a saepe aut. Illo sequi debitis placeat nostrum delectus recusandae velit, aliquid voluptas, a, et quaerat eveniet nobis fuga.
-    </div>
+    <Router />
   )
 }
 
